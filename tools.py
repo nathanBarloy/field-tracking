489925ecd3d8dc100716dc6fc7f4d84d5fea0ddb
Nathan Barloy <nbarloy@gmail.com>
2023-04-12 21:35:23 +0200

    Organize code in a class, and comment code

--- tools.py
+++ tools.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import time
 
 
 def calibrate(frame, output_dim):
@@ -43,6 +44,8 @@ def calibrate(frame, output_dim):
             if len(positions) > 0:
                 positions.pop()
     
+    cv2.destroyWindow("Callibration")
+
     # organisation of the corners
     positions.sort(key=lambda p: p[0])
     left = positions[:2]
@@ -71,3 +74,99 @@ def apply_transform_to_point(mat, point):
     in_vect = np.array([point[0], point[1], 1]).T
     out_vect = np.dot(mat, in_vect)
     return [out_vect[0]/out_vect[2], out_vect[1]/out_vect[2]]
+
+
+class VideoPlayer:
+    """
+    This class can be used to manipulate a video stream.
+
+    Attributes:
+    video : cv2.VideoCapture
+        The video OpenCV object.
+    paused : bool
+        True if the video is paused.
+    elapsed_time : float
+        Time to wait between two frames (in ms).
+    previous_frames : List[cv2.Image]
+        List of the previous frames. The last frame in the list is the current one.
+    next_frames : List[cv2.Image]
+        List of the next frames to play before getting new ones.
+    buffer_size : int
+        Max size of the previous_frame attribute.
+    field_dim : Tuple(float, float)
+        Dimensions of the watched field. Should be (width, height).
+    transform_matrix : numpy.array
+        The transformation matrix for the field.
+    
+    Methodes:
+    read_frame() -> bool
+        Read and show the next frame. Return False if it is the end of the video.
+    rewind_frame() -> None
+        Show the previous frame.
+    play() -> None
+        Start the video player.
+    """
+
+    def __init__(self, path, buffer_size, field_dim):
+        self.video = cv2.VideoCapture(path)
+        if not self.video.isOpened():
+            raise ValueError(f"Could not open video at path '{path}'.")
+        self.paused = True
+        fps = self.video.get(cv2.CAP_PROP_FPS)
+        self.elapsed_time = 1000/fps
+        self.previous_frames = [] # could be faster with a linked list
+        self.next_frames = []
+        self.buffer_size = buffer_size
+        self.field_dim = field_dim
+    
+    def read_frame(self):
+        if len(self.next_frames) == 0:
+            ret, frame = self.video.read()
+        else:
+            ret, frame = True, self.next_frames.pop()
+        if not ret:
+            return False
+        
+        if len(self.previous_frames) >= self.buffer_size:
+            self.previous_frames.pop(0)
+        self.previous_frames.append(frame)
+        cv2.imshow("Video", frame)
+        return True
+
+    def rewind_frame(self):
+        if len(self.previous_frames) > 1:
+            self.next_frames.append(self.previous_frames.pop())
+            cv2.imshow("Video", self.previous_frames[-1])
+
+    def start(self):
+        self.read_frame()
+        self.transform_matrix = calibrate(self.previous_frames[-1], self.field_dim)
+
+        old_time = time.time()
+        while self.video.isOpened():
+            if not self.paused:
+                if not self.read_frame():
+                    break
+
+            new_time = time.time()
+            to_wait = int(self.elapsed_time - 1000*(new_time-old_time))
+            key = cv2.waitKey(max(to_wait, 1))
+            old_time = new_time
+            if key == ord('q'):
+                break
+            if key == ord(' '):
+                self.paused = not self.paused
+            if key == ord('a'):
+                self.elapsed_time /=0.75
+            if key == ord('z'):
+                self.elapsed_time *=0.75
+            if key == ord('p'):
+                if self.paused:
+                    if not self.read_frame():
+                        break
+            if key == ord('o'):
+                if self.paused:
+                    self.rewind_frame()
+
+        self.video.release()
+        cv2.destroyAllWindows()
